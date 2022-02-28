961d8d90a380afb5ea4ab9948bcafe248654a43c
Nikita Kharlap <harlap19@gmail.com>
2022-02-28 11:50:05 +0300

    make header

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,47 @@ import * as functions from './modules/functions.js';
 
 console.log('Hello world!');
 
-functions.func();
+// functions.func();
+
+
+// Проверяем на мобилке или ПК
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+}
+
+if (isMobile.any()) {
+    document.body.classList.add('_mobile');
+}
+else {
+    document.body.classList.add('_pc');
+}
+
+
+// Добавляем класс для активного бургер меню
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+
+}
